fe02f55eeefe1dbd43a90ae3fb5094dd37e23a8b
Paul Sanchez <paxasaval1003@gmail.com>
2018-07-05 11:54:19 -0500

    Subiendo trabajo

---
+++ JavaApplication20/src/javaapplication20/Estudiantes.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication20;
+
+/**
+ *
+ * @author User
+ */
+public class Estudiantes {
+    String nombre;
+    String apellido;
+    int edad;
+    double calificacion1;
+    double calificacion2;
+    double promedio;
+    public void Imprimir_reporte(){
+        System.out.printf("%s %s",nombre,apellido);
+        System.out.printf("Edad - %d\n", edad);
+        System.out.printf("calificacion 1: %.2f\nCalificacion 2: %.2f\n",calificacion1,calificacion2);
+        promedio=(calificacion1+calificacion2)/2;
+        System.out.printf("Promedio: %.2f\n",promedio);
+    }
+}
---
+++ JavaApplication20/src/javaapplication20/Principal.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication20;
+
+/**
+ *
+ * @author User
+ */
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String[] nombres = {"José", "María", "Ana", "Paul", "David"};
+        String[] apellidos = {"Yaguana", "Benitez", "Gaviria", "Davila", "Andrade"};
+        int[] edades = {20, 19, 20, 19, 25};
+        double[] c1 = {9.1, 10, 8, 7, 9.1};
+        double[] c2 = {6.1, 5, 10, 7.2, 4.1};
+        Estudiantes[] estudiantes1 = new Estudiantes[nombres.length];
+        for (int i = 0; i < estudiantes1.length; i++) {
+            Estudiantes e=new Estudiantes();
+            e.nombre=nombres[i];
+            e.apellido=apellidos[i];
+            e.edad=edades[i];
+            e.calificacion1=c1[i];
+            e.calificacion2=c2[i];
+            estudiantes1[i]=e;
+        }
+        for (int i = 0; i < estudiantes1.length; i++) {
+            estudiantes1[i].Imprimir_reporte();
+        }
+    }
+
+}
