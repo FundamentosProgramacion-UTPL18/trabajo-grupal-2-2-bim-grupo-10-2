5a63f0b0be62cdfe2831ae37185bc7df42584704
Paul Sanchez <paxasaval1003@gmail.com>
2018-07-05 12:07:19 -0500

    Agregando comentarios

--- JavaApplication20/src/javaapplication20/Estudiantes.java
+++ JavaApplication20/src/javaapplication20/Estudiantes.java
@@ -19,10 +19,8 @@ public class Estudiantes { //creamos  todos los atributos de la clase
     double promedio;
 
     public void Imprimir_reporte() { // creamos metodo para imprimir el reporte de cada estuiante
-        System.out.printf("%s %s", nombre, apellido);
         System.out.printf("*********************\n");
         System.out.printf("%s %s\n", nombre, apellido);
-
         System.out.printf("Edad - %d\n", edad);
         System.out.printf("calificacion 1: %.2f\nCalificacion 2: %.2f\n", calificacion1, calificacion2);
         promedio = (calificacion1 + calificacion2) / 2; // proceso para sacar el promemdio de las calificaciones
