dcd417dd3741f08431bc7e5d335de6e3b5f3ef61
Gerson Santos <gersantos1234@gmail.com>
2018-07-05 12:02:28 -0500

    Agregando trabajo gupal_2_2bim

--- JavaApplication20/src/javaapplication20/Estudiantes.java
+++ JavaApplication20/src/javaapplication20/Estudiantes.java
@@ -9,18 +9,18 @@ package javaapplication20;
  *
  * @author User
  */
-public class Estudiantes {
-    String nombre;
+public class Estudiantes { //creamos  todos los atributos de la clase
+    String nombre; 
     String apellido;
     int edad;
     double calificacion1;
     double calificacion2;
     double promedio;
-    public void Imprimir_reporte(){
+    public void Imprimir_reporte(){ // creamos metodo para imprimir el reporte de cada estuiante
         System.out.printf("%s %s",nombre,apellido);
         System.out.printf("Edad - %d\n", edad);
         System.out.printf("calificacion 1: %.2f\nCalificacion 2: %.2f\n",calificacion1,calificacion2);
-        promedio=(calificacion1+calificacion2)/2;
+        promedio=(calificacion1+calificacion2)/2; // proceso para sacar el promemdio de las calificaciones
         System.out.printf("Promedio: %.2f\n",promedio);
     }
 }
--- JavaApplication20/src/javaapplication20/Principal.java
+++ JavaApplication20/src/javaapplication20/Principal.java
@@ -15,23 +15,23 @@ public class Principal {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        String[] nombres = {"José", "María", "Ana", "Paul", "David"};
-        String[] apellidos = {"Yaguana", "Benitez", "Gaviria", "Davila", "Andrade"};
-        int[] edades = {20, 19, 20, 19, 25};
-        double[] c1 = {9.1, 10, 8, 7, 9.1};
+        String[] nombres = {"José", "María", "Ana", "Paul", "David"}; // Agregando valores cadena a la lista -- 
+        String[] apellidos = {"Yaguana", "Benitez", "Gaviria", "Davila", "Andrade"}; 
+        int[] edades = {20, 19, 20, 19, 25}; // Agregando vaores enteros a la lista --
+        double[] c1 = {9.1, 10, 8, 7, 9.1}; // Agregando valores decimales a la lista --
         double[] c2 = {6.1, 5, 10, 7.2, 4.1};
-        Estudiantes[] estudiantes1 = new Estudiantes[nombres.length];
-        for (int i = 0; i < estudiantes1.length; i++) {
-            Estudiantes e=new Estudiantes();
-            e.nombre=nombres[i];
+        Estudiantes[] estudiantes1 = new Estudiantes[nombres.length]; // se crea objeto  estudiantes 1 (lista)
+        for (int i = 0; i < estudiantes1.length; i++) { // funcion para recorre desde 0 hasta el valor de la  lista estudiantes 1 
+            Estudiantes e=new Estudiantes(); // Se crea objeto  esrudiantes (clase)
+            e.nombre=nombres[i]; // se agrega la variable e cada dato de la lista  
             e.apellido=apellidos[i];
             e.edad=edades[i];
             e.calificacion1=c1[i];
             e.calificacion2=c2[i];
             estudiantes1[i]=e;
         }
-        for (int i = 0; i < estudiantes1.length; i++) {
-            estudiantes1[i].Imprimir_reporte();
+        for (int i = 0; i < estudiantes1.length; i++) { //funcion para recorrer desde 0 hsta el valor de la lista estudiantes
+            estudiantes1[i].Imprimir_reporte(); // llamamos al método imprimir por cada elemento de la lista 
         }
     }
 
